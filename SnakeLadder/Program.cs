c444db92ad8d26ffd856d21d62e9df9ebfd9be1a
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-25 20:36:24 +0530

    [~Vivek~] Completed Use Case 4

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -13,32 +13,39 @@ namespace SnakeLadder
             Console.WriteLine("Let's Play\n");
             Console.WriteLine("Currently you are at 0 Position\n");
 
-            int winningPosition = 100, diceRoll, currentPlayerPosition = 0;
-            int[] snake = {1,3,5,67,87,92,95,98};
-            int[] ladder = {4,6,21,28,36,51,71,80};
+            int totalBoardNumber = 100, diceNumber, currentBoardPosition = 0;
+
 
             Random random = new Random();
-            diceRoll = random.Next(1, 7);
-            Console.WriteLine($"Dice Rolling Result : {diceRoll}");
-           
 
-            currentPlayerPosition+=diceRoll;
-
-            Console.WriteLine($"You are now at position : {currentPlayerPosition}");
-
-            if(Array.Exists(snake, x => x == currentPlayerPosition))
+            while (currentBoardPosition < totalBoardNumber)
             {
-                currentPlayerPosition-=diceRoll;
-                Console.WriteLine($"\nYou reached at top of Snake , So You moved {diceRoll} steps behind\n");
-            }
-            if(Array.Exists(ladder, x => x == currentPlayerPosition))
-            {
-                Console.WriteLine($"You reached at bottom of Ladder , So You moved {diceRoll} steps ahead\n");
-                currentPlayerPosition+=diceRoll;
-            }
-            
+                int[] snake = { 1, 3, 5, 67, 87, 92, 95, 98 };
+                int[] ladder = { 4, 6, 21, 28, 36, 51, 71, 80 };
+                diceNumber = random.Next(1, 7);
+                Console.WriteLine($"\nDice Rolling Result : {diceNumber}");
 
-            Console.WriteLine($"\nYou are now at position : {currentPlayerPosition}");
+                currentBoardPosition += diceNumber;
+
+                Console.WriteLine($"\nYou are now at position : {currentBoardPosition}");
+
+                if (Array.Exists(snake, x => x == currentBoardPosition))
+                {
+                    currentBoardPosition -= diceNumber;
+                    Console.WriteLine($"\nYou reached at top of Snake , So You moved {diceNumber} steps behind");
+                }
+                if (Array.Exists(ladder, x => x == currentBoardPosition))
+                {
+                    Console.WriteLine($"\nYou reached at bottom of Ladder , So You moved {diceNumber} steps ahead");
+                    currentBoardPosition += diceNumber;
+                }
+
+                if (currentBoardPosition < 0)
+                {
+                    currentBoardPosition = 0;
+                }
+                Console.WriteLine($"\nYou are now at position : {currentBoardPosition}");
+            }
 
             Console.ReadLine();
 
