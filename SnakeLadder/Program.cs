e8fa3918437c142a7a54df20f6d1f175e7ea154c
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-25 15:47:02 +0530

    [~Vivek~] Completed UC1

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -1,12 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+using System.Security.Cryptography.X509Certificates;
+
 namespace SnakeLadder
 {
     class Program
     {
+        public const int winningPosition = 10;
+        int dice_score = 0;
+        static int attempt_number = 0;
+        int dieRoll;
+        static int current_position = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Snake Ladder Problem");
+            Console.WriteLine("========== Welcome To Snake and Ladder ===========");
+            Console.WriteLine($"Your Attempt No is: {attempt_number}");
+            Console.WriteLine($"Your current position is: {current_position}");
         }
     }
 }
