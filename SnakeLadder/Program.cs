3e5fde616c59e94aeb54d1d5c1a608752956ce47
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-25 16:27:04 +0530

    [~Vivek~] Completed UC2 for Rolling a dice with random number

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -8,14 +8,18 @@ namespace SnakeLadder
     {
         public const int winningPosition = 10;
         int dice_score = 0;
-        static int attempt_number = 0;
+        static int attempt_number = 1;
         int dieRoll;
         static int current_position = 0;
+
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int current_dice_number = random.Next(1, 7);
             Console.WriteLine("========== Welcome To Snake and Ladder ===========");
             Console.WriteLine($"Your Attempt No is: {attempt_number}");
-            Console.WriteLine($"Your current position is: {current_position}");
+            Console.WriteLine($"You got {current_dice_number} number on dice");
+            Console.WriteLine($"Your current position is: {current_position + current_dice_number}");
         }
     }
 }
