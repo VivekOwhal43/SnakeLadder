8a1be2333db9dae16bc389dcf791f1f5577bd0b1
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-25 20:51:55 +0530

    [~Vivek~] Completed Use Csae 5

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -23,7 +23,7 @@ namespace SnakeLadder
                 int[] snake = { 1, 3, 5, 67, 87, 92, 95, 98 };
                 int[] ladder = { 4, 6, 21, 28, 36, 51, 71, 80 };
                 diceNumber = random.Next(1, 7);
-                Console.WriteLine($"\nDice Rolling Result : {diceNumber}");
+                Console.WriteLine($"\n\n\nAfter rolling Dice,The number display on the Dice is : {diceNumber}");
 
                 currentBoardPosition += diceNumber;
 
@@ -39,16 +39,19 @@ namespace SnakeLadder
                     Console.WriteLine($"\nYou reached at bottom of Ladder , So You moved {diceNumber} steps ahead");
                     currentBoardPosition += diceNumber;
                 }
-
+                if (currentBoardPosition > 100)
+                {
+                    Console.WriteLine("You are out of range");
+                    currentBoardPosition -= diceNumber;
+                }
                 if (currentBoardPosition < 0)
                 {
                     currentBoardPosition = 0;
                 }
+
                 Console.WriteLine($"\nYou are now at position : {currentBoardPosition}");
             }
-
             Console.ReadLine();
-
         }
     }
 }
